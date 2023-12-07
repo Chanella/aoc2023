bcec1ef7bb844f4c5d6b6cc071cc89a64cad308c
Chanella <thethelia1@gmail.com>
2023-12-07 23:52:31 +0100

    almost done

--- exo7part1/exo7part1/Program.cs
+++ exo7part1/exo7part1/Program.cs
@@ -9,105 +9,117 @@ using System.Net.Http.Headers;
 
 public class hand
 {
-    public List<char> cards;
+    public List<int> cards;
     public int bid;
     int type;
 
-    public  hand()
-    {
-        cards = new List<char>();
-        bid = 0;
-    }
     public hand(List<char> c, int b)
     {
-        this.cards = c;
+        this.cards = Convert(c);
         bid = b;
-        type = 0;
+        type = assigntype();
+    }
+
+    public List<int> Convert(List<char> c)
+    {
+        List<int> result = new List<int>();
+        foreach(char ch in c) 
+        {
+            switch (ch)
+            {
+                case 'A':
+                    result.Add(14);
+                    break;
+                case 'K':
+                    result.Add(13);
+                    break;
+                case 'Q':
+                    result.Add(12);
+                    break;
+                case 'J':
+                    result.Add(11);
+                    break;
+                case 'T':
+                    result.Add(10);
+                    break;
+                default: 
+                    result.Add(Int32.Parse(ch.ToString())); 
+                    break;
+             }
+        }
+        return result;
     }
 
     public int assigntype()
     {
-        List<char> list = new List<char>();
-        list = cards;
+        List<string> list = new List<string>();
+        list = this.cards.Select(x=>x.ToString()).ToList();
+        this.type = 0;
 
-        int a = 0;
-
-        int t = list.FindAll(x => x == list[a]).Count();
-
-        if (t == 5)
+        list.RemoveAll(x => x == cards[0].ToString());
+        // 5 pareils
+        if (list.Count == 0 )
         {
-            this.type = 6;
-            return this.type;
+           this.type = 6;
+           return this.type;
         }
-        else if (t == 4)
+        // 4 pareils
+        if (list.Count == 1)
         {
             this.type = 5;
             return this.type;
         }
-        else
-        {
-            list.Remove(list[0]);
-            // il y a 3 pareils
-            if (list.Count == 2)
+
+         // il y a 3 pareils
+         if (list.Count == 2)
+         {
+            this.type = 3;
+            if (list[0] == list[1]) this.type ++;
+
+            return this.type;
+         }
+   
+         // il y a deux pareils
+         if (list.Count == 3)
+         {
+            this.type = 1;
+            string s = list[0];
+            list.RemoveAll(x => x == s);
+            if (list.Count == 1)
             {
-                //check s'il reste 2 pareils ou pas
-                if (list[0].Equals(list[1]))
-                    this.type = 4;
-                else this.type = 3;
-                
-                return this.type;
+                this.type ++;
             }
-             while (list.Count > 1) 
+            if(list.Count == 2) 
+            { 
+                if (list[0] == list[1]) this.type++; 
+            }
+            return this.type;
+         }   
+         if (list.Count == 4) 
+         {
+            string s = list[0];
+            list.RemoveAll(x => x == s);
+
+            if (list.Count == 0) {
+                this.type = 6;
+            }
+            if (list.Count == 1) { this.type = 3; }
+            if (list.Count == 2) 
             {
-                //t = list.FindAll(x => x == list[0]).Count();
-                
-                list.Remove(list[0]);
-                if (list.Count == 0)
-                {
-                    this.type = 6;
-                    return this.type;
-                }
-                if (list.Count == 1)
-                {
-                    this.type = 5;
-                    return this.type;
-                }
-
-                // il y a 3 pareils
-                if (t == 3)
-                {
-                   //check s'il reste 2 pareils ou pas
-                   if (list[0].Equals(list[1]))
-                      this.type = 4;
-                        else this.type = 3;
-
-                        return this.type;
-                }
-                    // il y a deux pareils
-                    if (list.Count == 3)
-                {
-                    
-                    if (t == 3) { this.type = 4; return this.type; }
-                    if (t == 2) { this.type = 2; return this.type; }
-                    if (t == 1 ) { list.Remove(list[0]); }
-                }
-                if (list.Count == 4)
-                {
-                    t = list.FindAll(x => x == list[a]).Count();
-                    if (t == 3) { this.type = 4; return this.type; }
-                    if (t == 2) { this.type = 2; return this.type; }
-                }
+                this.type = 1;
+                if (list[0] == list[1]) this.type++;
             }
-            
+            if (list.Count == 3) 
+            {
+                s = list[0];
+                list.RemoveAll(x => x == s);
+                if (list.Count == 0) this.type = 3;
 
-        
-        }
-
-
-
-
-        return type;
-
+                if (list.Count == 1) this.type = 1; 
+                if (list.Count == 2) if (list[0] == list[1]) this.type = 1;
+            }
+         }
+        return this.type;
     }
 
     public int getType { get { return type; } }
@@ -116,7 +128,7 @@ public class hand
     {
         return bid;
     }
-    public char getcard(int c)
+    public int getcard(int c)
     { return cards[c]; }
     
 }
@@ -127,7 +139,7 @@ internal class Program
     private static void Main(string[] args)
     {
         string line;
-        int way = 1;
+        double way = 0;
         List<hand> cards = new List<hand>();
 
         try
@@ -153,25 +165,19 @@ internal class Program
                      * 2 cartes pareilles solo => 1
                      * nada => 0
                      * */
-
-                    if (cards.Count == 0) 
-                    { 
-
-                        cards.Add(h); 
-                    }
-
-                    else
-                    { 
-                        foreach (hand hd in cards)
-                        {
-
-                        }
-                    }
-
-
-
+                    
                     cards.Add(h);
                     line = sr.ReadLine();
+                }
+
+                IEnumerable<hand> query = from hands in cards
+                                            orderby hands.getType, hands.cards[0], hands.cards[1], hands.cards[2], hands.cards[3], hands.cards[4]
+                                            select hands;
+                int i = 1;
+                foreach (hand h in query)
+                {
+                    way += i * h.bid;
+                    i++;
                 }
 
                 Console.WriteLine("le plus petit est " + way);
